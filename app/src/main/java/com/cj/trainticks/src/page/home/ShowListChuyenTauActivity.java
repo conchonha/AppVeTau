475531cd2457ac1c839cb39c0be45145e5384b07
sangthaiba <kzumi2110>
2021-01-06 08:37:43 +0700

    chon chuyen tau 50%

--- app/src/main/java/com/cj/trainticks/src/page/home/ShowListChuyenTauActivity.java
+++ app/src/main/java/com/cj/trainticks/src/page/home/ShowListChuyenTauActivity.java
@@ -1,9 +1,16 @@
 package com.cj.trainticks.src.page.home;
 
+import android.content.Intent;
+import android.view.View;
+
 import com.cj.trainticks.R;
 import com.cj.trainticks.base.BaseActivity;
+import com.cj.trainticks.utils.Constain;
+import com.google.gson.JsonArray;
 
-public class ShowListChuyenTauActivity extends BaseActivity {
+import org.json.JSONArray;
+
+public class ShowListChuyenTauActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected int getContentView() {
         return R.layout.show_list_chuyen_tau_activity;
@@ -16,11 +23,22 @@ public class ShowListChuyenTauActivity extends BaseActivity {
 
     @Override
     protected void onListenerClicked() {
-
+        findViewById(R.id.img_back).setOnClickListener(this);
     }
 
     @Override
     protected void onInit() {
+//        Intent intent = getIntent();
+//        String reponse = intent.getStringExtra(Constain.keyListChuyenTau);
+//        JSONArray jsonArray 
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_back:
+                finish();
+                break;
+        }
     }
 }
